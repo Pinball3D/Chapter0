0c15bb536eee7f11ec77b7b5f003d03ecce44e87
Andrew Smiley <smiley29kid@gmail.com>
2024-09-04 14:26:05 -0400

    Chapter 0 9/4/2024

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
 
 public class Main {
     public static void main(String[] args) {
@@ -8,6 +12,13 @@ public class Main {
         JTextField name = new JTextField();
         name.setBounds(50,30,100, 30);
         app.add(name);
+        try {
+            BufferedImage myPicture = ImageIO.read(new File("./image.jpg"));
+            JLabel picLabel = new JLabel(new ImageIcon(myPicture));
+            app.add(picLabel);
+        } catch(Exception e) {
+
+        }
         JButton enter = new JButton("Enter");
         enter.setBounds(50,65,100, 30);
         app.add(enter);
